ea7ed39d1110aacea6f4262e5d096b7f3458eba6
SajjadGholamipour <30565283+SajjadGholamipour@users.noreply.github.com>
2024-02-29 14:36:20 +0330

    Update setup-kubetools.sh
    Installing neccessary packages and instruction of adding kubernetes   urls to apt's sources.list are added

--- setup-kubetools.sh
+++ setup-kubetools.sh
@@ -22,10 +22,13 @@ then
 	br_netfilter
 EOF
 	
-	sudo apt-get update && sudo apt-get install -y apt-transport-https curl
-	curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-	cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-	deb https://apt.kubernetes.io/ kubernetes-xenial main
+	#sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+	#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+	#cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+	#deb https://apt.kubernetes.io/ kubernetes-xenial main
+ 	sudo apt-get install -y gpg apt-transport-https ca-certificates curl
+  	curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+   	echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 EOF
 
 	sudo apt-get update
